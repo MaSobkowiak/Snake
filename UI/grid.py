307c4fc94bdcf68a4b97927d5d77ee2d8aa75450
Maciej Sobkowiak <macsob3@st.amu.edu.pl>
2019-05-02 13:54:50 +0200

    add window and grid controll

---
+++ UI/grid.py
@@ -0,0 +1,81 @@
+import pygame as pg
+import numpy as np
+
+
+class Grid:
+
+    def __init__(self, cols: int, rows: int):
+        self.table = [[Node(row, col)
+                       for col in range(cols)]
+                      for row in range(rows)]
+        self.cols = cols
+        self.rows = rows
+
+    def draw_map(self, screen: "PyGame screen"):
+        """Draws whole map"""
+        screen.fill(Node.BLACK)
+        for row in self.table:
+            for node in row:
+                node.draw(screen)
+
+    def change_field(self, row: int, col: int, f_type: int):
+        self.table[row][col].change_field_type(f_type)
+
+    def draw_node(self, screen, row: int, col: int):
+        self.table[row][col].draw(screen)
+
+
+class Node:
+    # define rectangles dimensions
+    r_width = 20
+    r_height = 20
+    r_margin = 5
+
+    # define some basic colors
+    # TODO: change to Enum
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
+    BLUE = (0, 0, 255)
+    ORANGE = (255, 165, 0)
+    PINK = (199,21,133)
+
+    def __init__(self, row: int, col: int,
+                 field_type: int = 0):
+        self.row = row
+        self.col = col
+        self.field_type = field_type
+
+
+
+    def draw(self, screen):
+        color = self.get_field_color()
+        col = self.col
+        row = self.row
+        width = self.r_width
+        height = self.r_height
+        margin = self.r_margin
+        # rect -> (left, top, width, height)
+        # draw.rect(surface, color, rect, margin)
+        pg.draw.rect(screen, color,
+                     ((col * (width + margin)) + margin,
+                      (row * (height + margin)) + margin,
+                      width, height))
+        pg.display.flip()  # refresh screen
+
+    def change_field_type(self, field_type: int):
+        self.field_type = field_type
+
+    def get_field_color(self) -> tuple:
+        """Gets the color tuple of field"""
+        if self.field_type == 0:
+            return self.GREEN
+        elif self.field_type == 1:
+            return self.RED
+        elif self.field_type == 2:
+            return self.BLUE
+        elif self.field_type == 3:
+            return self.ORANGE
+        elif self.field_type == 4:
+            return self.PINK
---
+++ UI/window.py
@@ -0,0 +1,69 @@
+import pygame as pg
+import numpy as np
+import random
+from UI.grid import Grid, Node
+from UI.Apath import a_path
+
+
+class Window():
+    def __init__(self, grid: Grid,start: (int,int),end: (int,int),mode: int):
+        pg.init()   # pylint: disable=no-member
+        # setup window
+        pg.display.set_caption('Inteligentna śmieciarka')
+        
+        self.start = start
+        self.end = end
+        self.grid = grid
+
+        # assign to variables for brevity
+        cols = self.grid.cols
+        rows = self.grid.rows
+        width = Node.r_width
+        height = Node.r_height
+        margin = Node.r_margin
+
+
+        screen_width = cols * (width + margin) + 2 * margin
+        screen_height = rows * (height + margin) + 2 * margin
+
+        self.screen = pg.display.set_mode([screen_width, screen_height])
+
+        self.end = False
+
+        self.clock = pg.time.Clock()
+        
+
+        def obstacles(self, grid: Grid,option: int):
+            if option == 1:
+                for x in range(len(grid.table)*13):
+                    grid.change_field(random.randint(1,len(grid.table)-2),random.randint(1,len(grid.table)-2),3)
+            elif option == 2:
+                for x in range (13):
+                    grid.change_field(x,14,3)
+                for x in range (8):
+                    grid.change_field(12,x+6,3)
+                
+
+
+        obstacles(self,grid,mode)
+        grid.change_field(start[0], start[1], 1)
+        grid.change_field(end[0], end[1], 2)
+
+        #draw starting map
+        self.grid.draw_map(self.screen)
+
+        #copy table
+        array = [[self.grid.table[col][row] for row in range(cols)] for col in range(rows)]
+        path = a_path(array,(start[0],start[1]),(end[0],end[1]))
+        print("Path:",path)
+
+        #draw movement of garbage truck
+        for index, t  in enumerate(path):
+            x,y =t
+            if index != 0:
+                self.grid.change_field(path[index-1][0],path[index-1][1],4)
+            self.grid.change_field(x, y, 1)
+            self.grid.draw_node(self.screen, path[index-1][0],path[index-1][1])
+            self.grid.draw_node(self.screen, x, y)
+            pg.time.delay(500)
+        pg.quit()   # pylint: disable=no-member
